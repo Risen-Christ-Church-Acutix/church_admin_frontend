cf2f6d67f8c3a2661c3b0436ef6d62501b27844f
sujan <sujanpugazhendhi@gmail.com>
2025-06-14 17:18:09 +0000

    [Fix]: apiUrl for local

--- src/api-handler/api-handler.js
+++ src/api-handler/api-handler.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const isProduction = import.meta.env.MODE === 'production';
-const apiUrl = isProduction ? import.meta.env.VITE_API_URL : import.meta.env.NON_PRODUCTION_API_URL;
+const apiUrl = isProduction ? import.meta.env.VITE_API_URL : import.meta.env.VITE_NON_PRODUCTION_API_URL;
 
 const axiosInstance = axios.create({
   baseURL: apiUrl,
