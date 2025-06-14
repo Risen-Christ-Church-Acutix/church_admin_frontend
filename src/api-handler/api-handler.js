6116dc55937e1cf220fe4af7f05f5a6d387f1199
sujan <sujanpugazhendhi@gmail.com>
2025-06-14 16:06:16 +0000

    [UPDATE]: api-url

--- src/api-handler/api-handler.js
+++ src/api-handler/api-handler.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+const isProduction = import.meta.env.MODE === 'production';
+const apiUrl = isProduction ? import.meta.env.VITE_API_URL : import.meta.env.NON_PRODUCTION_API_URL;
+
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:4000/', 
+  baseURL: apiUrl,
   headers: {
     'Content-Type': 'application/json',
   },
